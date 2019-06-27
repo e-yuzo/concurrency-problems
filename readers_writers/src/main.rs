423ab7423d08b313412922559b7656c2a232d6e1
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2019-06-27 17:39:57 -0300

    fix gitignore

--- readers_writers/src/main.rs
+++ readers_writers/src/main.rs
@@ -3,7 +3,6 @@ use std::sync::{Arc};
 use std::time::Duration;
 use std::sync::RwLock;
 
-
 struct Writer {
     name: String,
 }
@@ -93,5 +92,4 @@ fn main() {
     for handle in writers_handles {
         handle.join().unwrap();
     }
-
 }
