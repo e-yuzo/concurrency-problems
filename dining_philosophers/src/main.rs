3da66d0b9faa5f91cb2ade27774b7dd4e54baf0d
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2019-07-03 00:12:37 -0300

    adding comments

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -2,18 +2,23 @@ use std::thread;
 use std::sync::{Mutex, Arc};
 use std::time::Duration;
 
+// struct que representa uma mesa com vários garfos
 struct Table {
     forks: Vec<Mutex<()>>,
 }
 
+// struct que representa os filósofos
 struct Philosopher {
     name: String,
+    // usize: tipo utilizado para indexar vetores (Table.forks)
     left: usize,
     right: usize,
 }
 
+// implementação da struct Philosopher
 impl Philosopher {
 
+    // função new: convenção para criação de instâncias
     fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
@@ -24,26 +29,34 @@ impl Philosopher {
 
     fn eat(&self, table: &Table) {
         loop {
+            // Judith Butler pega o garfo direito e esquerdo (nesta equência)
             if self.name == "Judith Butler" {
                 println!("{} is THINKING.", self.name);
-                thread::sleep(Duration::new(1, 0)); // Applies a 'simultaneity fudge factor'
+                // simulação do tempo que o filósofo gasta pensando
+                thread::sleep(Duration::new(1, 0));
 
                 println!("{} is HUNGRY.", self.name);
+                // filósofo tenta pegar os garfos
                 let _left = table.forks[self.right].lock().unwrap();
                 let _right = table.forks[self.left].lock().unwrap();
 
                 println!("{} is EATING.", self.name);
+                // simulação do tempo que o filósofo gasta comendo
                 thread::sleep(Duration::new(2, 0));
                 println!("{} is DONE EATING.", self.name);
+            // outros filósofos pegam o garfo esquerdo e direito (nesta sequência)
             } else {
                 println!("{} is THINKING.", self.name);
-                thread::sleep(Duration::new(1, 0)); // Applies a 'simultaneity fudge factor'
+                // simulação do tempo que o filósofo gasta pensando
+                thread::sleep(Duration::new(1, 0));
 
                 println!("{} is HUNGRY.", self.name);
+                // filósofo tenta pegar os garfos
                 let _left = table.forks[self.left].lock().unwrap();
                 let _right = table.forks[self.right].lock().unwrap();
 
                 println!("{} is EATING.", self.name);
+                // simulação do tempo que o filósofo gasta comendo
                 thread::sleep(Duration::new(2, 0));
                 println!("{} is DONE EATING.", self.name);
             }
@@ -53,6 +66,8 @@ impl Philosopher {
 }
 
 fn main() {
+    // ARC: atomic reference count: para compartilhar a table entre múltiplas threads
+    // Mutex: forma de controlar a concorrência
     let table = Arc::new(Table { forks: vec![
         Mutex::new(()),
         Mutex::new(()),
@@ -61,6 +76,7 @@ fn main() {
         Mutex::new(()),
     ]});
 
+    // criação dos filósofos com seus respectivos nomes e garfos
     let philosophers = vec![
         Philosopher::new("Judith Butler", 0, 1),
         Philosopher::new("Gilles Deleuze", 1, 2),
@@ -69,15 +85,18 @@ fn main() {
         Philosopher::new("Michel Foucault", 0, 4),
     ];
 
+    // handles das threads criadas
+    // into_iter(): criação de um iterator que adquire ownership de cada filósofo
     let handles: Vec<_> = philosophers.into_iter().map( |philosopher| {
         let table = table.clone();
-
+        // criação das threads
         thread::spawn(move || {
             philosopher.eat(&table);
         })
-
+    // coleção de elementos (handles) que a thread::spawn retorna
     }).collect();
 
+    // operação join() para cada handle retornado das threads
     for handle in handles {
         handle.join().unwrap();
     }
--- readers_writers/src/main.rs
+++ readers_writers/src/main.rs
@@ -17,10 +17,13 @@ impl Writer {
 
     fn write(&self, rw_lock: &RwLock<i32>) {
         loop {
+            // write(): acesso exclusivo
             if let Ok(mut write_guard) = rw_lock.write() {
+                // o read_guard retornado também implementa 'Deref'
                 *write_guard += 1;
                 println!("{} UPDATED value to {}", self.name, *write_guard);
             }
+            // quando o guard sai do escopo, escritor perde o lock
             thread::sleep(Duration::from_millis(1000));
         }
     }
@@ -41,9 +44,12 @@ impl Reader {
 
     fn read(&self, rw_lock: &RwLock<i32>) {
         loop {
+            // read() bloqueia se um escritor possui o write() lock
             if let Ok(read_guard) = rw_lock.read() {
+                // o read_guard retornado também implementa 'Deref'
                 println!("{} READ value {}", self.name, *read_guard);
             }
+            // quando o guard sai do escopo, leitor perde o lock
             thread::sleep(Duration::from_millis(500));
         }
     }
@@ -51,9 +57,13 @@ impl Reader {
 }
 
 fn main() {
+    // cria um i32 com valor inicial de 0
     let value:i32 = 0;
+    // ARC: atomic reference count: para compartilhar o rwLock entre múltiplas threads
+    // RwLock: permite um determinado número de leitores ou no máximo 1 escritor num determinado momento
     let rw_lock = Arc::new(RwLock::new(value));
 
+    // criação dos leitores
     let readers = vec![
         Reader::new("Judith Butler"),
         Reader::new("Gilles Deleuze"),
@@ -62,33 +72,41 @@ fn main() {
         Reader::new("Michel Foucault"),
     ];
 
+    // criação dos escritores
     let writers = vec![
         Writer::new("Socrates"),
         Writer::new("Plato"),
         Writer::new("Aristotle"),
     ];
 
-    let readers_handles: Vec<_> = readers.into_iter().map( |reader| {
+    // handles das threads criadas
+    // into_iter(): criação de um iterator que adquire ownership de cada leitor
+    let readers_handles: Vec<_> = readers.into_iter().map(|reader| {
         let rw_lock = rw_lock.clone();
-
+        // criação das threads
         thread::spawn(move || {
             reader.read(&rw_lock);
         })
-
+    // coleção de elementos (handles) que a thread::spawn retorna
     }).collect();
 
-    let writers_handles: Vec<_> = writers.into_iter().map( |writer| {
+    // handles das threads criadas
+    // into_iter(): criação de um iterator que adquire ownership de cada escritor
+    let writers_handles: Vec<_> = writers.into_iter().map(|writer| {
         let rw_lock = rw_lock.clone();
-
+        // criação das threads
         thread::spawn(move || {
             writer.write(&rw_lock);
         })
-
+    // coleção de elementos (handles) que a thread::spawn retorna
     }).collect();
 
+    // operação join() para cada handle retornado das threads 
     for handle in readers_handles {
         handle.join().unwrap();
     }
+
+    // operação join() para cada handle retornado das threads
     for handle in writers_handles {
         handle.join().unwrap();
     }
